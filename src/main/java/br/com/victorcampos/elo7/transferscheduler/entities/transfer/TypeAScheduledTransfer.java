ce563d8ca660955ce2a7b3cebca898fc7c7fb10a
Victor Campos <piva00@gmail.com>
2013-10-18 16:45:17 -0300

    Create type A transfer
    Add type A scheduled transfer
    Add tests for fee calculation on type A operations

---
+++ src/main/java/br/com/victorcampos/elo7/transferscheduler/entities/transfer/TypeAScheduledTransfer.java
@@ -0,0 +1,20 @@
+package br.com.victorcampos.elo7.transferscheduler.entities.transfer;
+
+public class TypeAScheduledTransfer extends ScheduledTransfer {
+
+    private static String TYPE = "A";
+
+    @Override
+    public String getType() {
+	return TYPE;
+    }
+
+    @Override
+    public int calculateFee() {
+	// TODO: rounding errors on int casting will be a problem?
+	int calculatedFee = (int) (getTransferAmount() * 0.03) + 2;
+
+	return calculatedFee;
+    }
+
+}
---
+++ src/test/java/br/com/victorcampos/elo7/transferscheduler/entities/transfer/TypeAScheduledTransferTest.java
@@ -0,0 +1,28 @@
+package br.com.victorcampos.elo7.transferscheduler.entities.transfer;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TypeAScheduledTransferTest {
+
+    @Test
+    public void testGetType() {
+	ScheduledTransfer typeAScheduledTransfer = new TypeAScheduledTransfer();
+	
+	assertEquals("A", typeAScheduledTransfer.getType());
+    }
+
+    @Test
+    public void testCalculateFee() {
+	int expectedFee = (int) (10000 * 0.03) + 2;
+	ScheduledTransfer typeAScheduledTransfer = new TypeAScheduledTransfer();
+	
+	// set transfer amount to $100.00
+	typeAScheduledTransfer.setTransferAmount(10000);
+	int calculatedFee = typeAScheduledTransfer.calculateFee();
+	
+	assertEquals(expectedFee, calculatedFee);
+    }
+
+}
