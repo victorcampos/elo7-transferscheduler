dd6330a66751fd5e8f02138327c792cd6caac91a
Victor Campos <piva00@gmail.com>
2013-10-21 13:53:17 -0200

    Get rid of Eclipse warning
    Add default serial version ID to ScheduledTransferMock to clear Eclipse warnings

--- src/test/java/br/com/victorcampos/elo7/transferscheduler/entities/transfer/ScheduledTransferMock.java
+++ src/test/java/br/com/victorcampos/elo7/transferscheduler/entities/transfer/ScheduledTransferMock.java
@@ -5,6 +5,8 @@ import org.joda.time.DateTime;
 import br.com.victorcampos.elo7.transferscheduler.InvalidArgumentException;
 
 public class ScheduledTransferMock extends ScheduledTransfer {
+    
+    private static final long serialVersionUID = 1L;
 
     public ScheduledTransferMock(String originAccount,
 	    String destinationAccount, int transferAmount,
